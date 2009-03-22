bb40e71154bd3a3aa637a81dfd162ee70e5483dd
mattwiebe3 <mattwiebe3@a10022e2-1735-11de-bcda-2bb9902f8029>
2009-03-22 23:17:54 +0000

    initial import

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+//remote version
+javascript:(function()%20{if(typeof%20jQuery=='undefined'){var%20jqit=document.createElement('script');jqit.type='text/javascript';jqit.src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';document.getElementsByTagName('head')[0].appendChild(jqit);}%20_my_script=document.createElement('script');_my_script.type='text/javascript';_my_script.src='http://font-friend.googlecode.com/svn/trunk/font-friend.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();
+
+//local test version
+javascript:(function()%20{if(typeof%20jQuery=='undefined'){var%20jqit=document.createElement('script');jqit.type='text/javascript';jqit.src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';document.getElementsByTagName('head')[0].appendChild(jqit);}%20_my_script=document.createElement('script');_my_script.type='text/javascript';_my_script.src='http://localhost/font-friend/font-friend.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();
