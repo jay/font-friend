8dbc17c64415c849b15bf75fb939a96851617c3a
Jay Satiro <raysatiro@yahoo.com>
2025-05-24 00:07:18 -0400

    Fix deprecated jquery size() method
    - Use .length instead of .size().
    
    Remember to purge CDN cache via https://www.jsdelivr.com/tools/purge
    
    Ref: davelab6/font-friend@29bbb994
    Ref: https://github.com/davelab6/font-friend
    Ref: https://github.com/zoghal/font-friend
    Ref: https://cdn.jsdelivr.net/gh/jay/font-friend@master/font-friend.js

--- font-friend-dev.js
+++ font-friend-dev.js
@@ -59,7 +59,7 @@
 		$ = window.jQuery;
 
 		// check if it's already been added. saves against weirdness if clicked again.
-		if ( $('#font-friend').size() !== 0 ) {
+		if ( $('#font-friend').length !== 0 ) {
 			return false;
 		}
 
@@ -326,7 +326,7 @@
 		}
 
 		// exit early if we already have a list
-		if ( existingUl.size() === 1 ) {
+		if ( existingUl.length === 1 ) {
 			existingUl.append(html);
 			return buildFamilies();
 		}
